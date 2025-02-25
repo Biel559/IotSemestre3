9ee8db1100bd024263c757cef983368206760261
Biel559 <bielcampopiano@gmail.com>
2025-02-24 22:18:33 -0300

    commit aula vue typescript

---
+++ Aula5-/domotica/src/views/Devices.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+// export default{
+//     name: "Devices",
+//     data() {
+//         return {
+//         contador: 0
+//         }
+//     },
+//     methods:{
+//         incrementar() {
+//             this.contador ++;
+//         },
+//         decrementar() {
+//             this.contador --;
+//         },
+//     } 
+// }
+
+import {ref} from 'vue';
+
+const contador = ref(0);
+const contar = ()=> {
+    contador.value +=1;
+    console.log(contador);
+}
+const descontar = ()=> {
+    contador.value -=1;
+    console.log(contador);
+}
+</script>
+
+<template>
+<h1>Hello Devices! Valor: {{ contador }}</h1>
+<button @click="contar">incrementar</button>
+<button @click="descontar">decrementar</button>
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ Aula5-/domotica/src/views/NotFound.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+ <h1>Not Found! 😂😂😂😂😂😂😂😂</h1>
+ <img src="https://s2.glbimg.com/Lz7AHbOCTWPg-uoubmWigSiBqCQ=/1200x/smart/filters:cover():strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2022/R/v/cYQVSDSt2ZORRKwiI3TA/carrossel-ponte-preta.jpg" alt="">
+</template>
+
+<style scoped lang="scss">
+
+</style>
